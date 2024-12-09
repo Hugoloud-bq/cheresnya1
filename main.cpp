800e87b176862ccfee98d402db3272faf7f3df8b
Hugoloud-bq <s.tchulyakov@yandex.ru>
2024-12-09 10:16:58 +0300

    Удалены все комментарии из кода

--- main.cpp
+++ main.cpp
@@ -13,16 +13,12 @@ int main(){
 	int massive[size]{5,1,2,6,1};
 	
 	int i_min{}, i_max{};
-	// i_min = min_index(massive,size);
-	// i_max = max_index(massive,size);
+	
 	std :: cout << "i_min: " << i_min << "\ti_max: " << i_max << '\n';
 	
 	
 	int _sum{};
 	int begin{}, int end{};
-	// begin = max(i_max,i_min);
-	// end = min(i_max,i_min);
-	// _sum = sum(massive,size,begin, end);
 	std :: cout << "sum: " << sum << '\n';
 	
 	return 0;
